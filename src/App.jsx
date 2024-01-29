865cac72ac2ec61fb3dd184b769da1d9fea68b8d
jayavardhan5555 <89379481+jayavardhan5555@users.noreply.github.com>
2024-01-29 20:22:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import About from "./components/About";
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <Skills />
+      <About />
+      <Projects />
+    </>
+  );
+};
+
+export default App;
